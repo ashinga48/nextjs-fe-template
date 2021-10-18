464b2da8e40c35a91433e5e8296dd7953ace24fd
Ravi Teja Kesanam <kesanam.ravi@gmail.com>
2021-10-18 16:43:02 +1100

    code cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import Layout from "@components/Layout";
-import { Infocard } from "@resanec/gui.sp.infocard";
-import { home_invite_info_card, home_create_plan_info_card, home_find_service_provider_info_card } from "@constants/*";
-import { LoveIcon } from "@resanec/gui.ui.icons";
-import { Container, Flex } from "@resanec/gui.ui.themewrapper";
+import { SampleRecord } from "@constants/*";
 
 const Home: React.FC = () => {
   return (
     <Layout>
-      
-      <Container maxW="container.xl" pt={5} mb={5}>
-        <Flex mb={5}>
-          <Infocard {...home_create_plan_info_card} bgColor={'orange.200'} p={6} color="gray.900" borderRadius="xl" icon={<LoveIcon color="white" />} width={'100%'} />
-        </Flex>
-        <Flex gridGap={5}>
-          <Infocard {...home_invite_info_card} bgColor={'green.600'} p={6} color="white" borderRadius="xl" width={'100%'} />
-          <Infocard {...home_find_service_provider_info_card} bgColor={'purple.100'} p={6} color="gray.900" borderRadius="xl" icon={<LoveIcon color="white" />} width={'100%'} />
-        </Flex>
-      </Container>
+      Hello there
     </Layout>
   );
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 import GlobalStyle from "@styles/globalStyles";
 import { ThemeWrapper } from "@resanec/gui.ui.themewrapper";
-import { Footer } from "@resanec/gui.sp.footer";
-import { Header } from "@resanec/gui.sp.header";
-import { FooterMenu, HeaderMenu } from "@constants/*";
 
-const Layout: React.FC = ({ children }) => {
+type LayoutProps = {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC = ({ children }: LayoutProps) => {
   return (
     <ThemeWrapper>
       <GlobalStyle />
-
-      <Header menu={HeaderMenu} onSignIn={()=> alert(33)} onSignUp={()=> alert(44)} />
-
       <div id="content">
         {children}
       </div>
-
-      <Footer menu={FooterMenu} />
     </ThemeWrapper>
   );
 }
